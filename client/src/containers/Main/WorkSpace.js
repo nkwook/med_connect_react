b9a4d2dc532897937700097d4ca96e72e7331fca
Wook <ruddnrdl@kaist.ac.kr>
2020-01-21 18:52:48 +0900

    배고파

---
+++ client/src/containers/Main/WorkSpace.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {CurrentWrapper} from 'components/Main'
+// import { WorkSpace } from '.';
+class WorkSpace extends Component{
+    
+    render(){
+        return(
+            <div style={{marginLeft: '100px', marginTop: '40px'}}>
+                
+                <CurrentWrapper >
+
+                </CurrentWrapper>
+            </div>
+        )
+    }
+}
+
+
+export default WorkSpace;
